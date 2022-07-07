e3b8bb40d81e14db824993893075d00378e04376
Tomáš Hobza <thomashobza@ids.company>
2022-07-07 11:59:00 +0200

    add: TypeWriter component

---
+++ src/lib/components/TypeWriter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let speed = 100;
+	export let delay = 0;
+	export let text: string = '';
+	export let loop = false;
+
+	let p = 0;
+	$: shownText = text.slice(0, p);
+	let interval;
+
+	onMount(() => {
+		setTimeout(() => {
+			interval = setInterval(() => {
+				p++;
+			}, speed);
+		}, delay);
+
+		if (loop)
+			setTimeout(() => {
+				clearInterval(interval);
+				p = 0;
+
+				setTimeout(() => {
+					interval = setInterval(() => {
+						p++;
+					}, speed);
+				}, delay);
+			}, 30000);
+	});
+</script>
+
+{@html shownText}
